1145fab264c11f16f2a730547c3a71b8a252fbf2
Samnang2k <Samnang2k24@outlook.com>
2024-10-24 23:04:49 +0700

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
         <section className="resume-section p-3 p-lg-5 " id="experience">
           <div className="row my-auto">
             <div className="col-12">
-              <h2 className="  text-center">Educational Background</h2>
+              <h2 className="  text-center">Education</h2>
               <div className="mb-5 heading-border"></div>
             </div>
             <div className="resume-item col-md-6 col-sm-12 ">
